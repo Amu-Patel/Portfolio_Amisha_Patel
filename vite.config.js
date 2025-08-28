840bf75e1a79ce1457e4c32840dd0709fcef7986
AMISHA PATEL <147070843+Amu-Patel@users.noreply.github.com>
2025-08-28 14:26:46 +0530

    Deploy production build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "./", // ensures assets load correctly from root
+  base: "/Portfolio_Amisha_Patel/", 
   plugins: [react()],
 })
