bf49437f8ae50044a98c0f0999ed5500762b8fa5
AMISHA PATEL <147070843+Amu-Patel@users.noreply.github.com>
2025-08-28 14:22:42 +0530

    Update site

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Portfolio_Amisha_Patel/",
+  base: "./", // ensures assets load correctly from root
   plugins: [react()],
 })
